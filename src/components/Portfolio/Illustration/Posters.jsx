85c7ed345f7312ed6238243a78162f20fbbfb9a3
SalemDrenay <salemdrenay@gmail.com>
2021-02-03 11:15:46 +0100

    posters page css done

--- src/components/Portfolio/Illustration/Posters.jsx
+++ src/components/Portfolio/Illustration/Posters.jsx
@@ -4,6 +4,7 @@ import Carousel, { Modal, ModalGateway } from "react-images";
 
 import Navbar from '../../reusables/Navbar';
 import { photos } from "../../galleries/gallery-posters";
+import '../../../assets/css/Portfolio/Illustration/Posters.css';
 
 function Posters() {
   const [currentImage, setCurrentImage] = useState(0);
@@ -23,23 +24,25 @@ function Posters() {
     <div>
       <Navbar />
       <div className="posters">
-        <h2>Affiches &amp; couvertures</h2>
-        <div className="posters-gallery">
-          <Gallery photos={photos} onClick={openLightbox} />
-          <ModalGateway>
-            {viewerIsOpen ? (
-              <Modal onClose={closeLightbox}>
-                <Carousel
-                  currentIndex={currentImage}
-                  views={photos.map(x => ({
-                    ...x,
-                    srcset: x.srcSet,
-                    caption: x.title
-                  }))}
-                />
-              </Modal>
-            ) : null}
-          </ModalGateway>
+        <div className="posters-container">
+          <h2>Affiches &amp; couvertures</h2>
+          <div className="posters-gallery">
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+              {viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                  <Carousel
+                    currentIndex={currentImage}
+                    views={photos.map(x => ({
+                      ...x,
+                      srcset: x.srcSet,
+                      caption: x.title
+                    }))}
+                  />
+                </Modal>
+              ) : null}
+            </ModalGateway>
+          </div>
         </div>
       </div>
     </div>
