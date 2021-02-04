e5d8884fbb79aa83c69cc47a1136e3db3ae58648
SalemDrenay <salemdrenay@gmail.com>
2021-02-04 15:20:43 +0100

    drenay page done

---
+++ src/components/galleries/Conceptart/gallery-drenay-archi.jsx
@@ -0,0 +1,103 @@
+import ArenaExt from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/arena1.jpg';
+import ArenaPiste from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/arena2.jpg';
+import ArenaCouloir from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/main.png';
+import ArenaLobby from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/lobby_decor.jpg';
+import ArenaConcept from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/arena_concepts.jpg';
+
+import Dedale from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/dedale.jpg';
+
+import Maar from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/maar.jpg';
+import ForgePinces from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/forge_feuXpinces.jpg';
+import ForgeFeu from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/forge-test.jpg';
+
+import Market from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/market_decor.jpg';
+import Workshop from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/workshop_golemfactory.png';
+import ComptoirLegions from '../../../assets/images/Portfolio/illu/conceptart/drenay/archi/comptoirlegions.jpg';
+
+export const photos = [
+    {
+      src: ArenaExt,
+      width: 1,
+      height: 1,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ArenaPiste,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ArenaConcept,
+      width: 3,
+      height: 4,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ArenaCouloir,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ArenaLobby,
+      width: 1,
+      height: 1,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: Dedale,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: Maar,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ForgePinces,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ForgeFeu,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: Market,
+      width: 4,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: Workshop,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: ComptoirLegions,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    }
+  ];
+  
---
+++ src/components/galleries/Conceptart/gallery-drenay-worldbuilding.jsx
@@ -0,0 +1,119 @@
+import EleyrasHumains from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_hum.jpg';
+import EleyrasDrakes from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_drakes.jpg';
+import EleyrasLady from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_lady.jpg';
+import EleyrasReine from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_reine.jpg';
+import EleyrasAmbassKhaneyras from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_ambassadeurs1.jpg';
+import EleyrasAmbassLakeyras from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_ambassadeurs2.jpg';
+import EleyrasTrain from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_train.jpg';
+import EleyrasPaysage from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eleyras_view.jpg';
+
+import Khaneyras from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/khaneyras.jpg';
+
+import LakeyrasArmures from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/lakeyras_armures.jpg';
+import LakeyrasAutres from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/lakeyras_autres.jpg';
+
+import EyrasNature from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eyras_nat.jpg';
+import EyrasUrbain from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/eyras_urb.jpg';
+import EyrasMarchand from '../../../assets/images/Portfolio/illu/conceptart/drenay/worldbuilding/marchand.jpg';
+
+export const photos = [
+    {
+      src: EleyrasHumains,
+      width: 3,
+      height: 4,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasDrakes,
+      width: 3,
+      height: 4,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasLady,
+      width: 2,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasReine,
+      width: 2,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasAmbassKhaneyras,
+      width: 2,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasAmbassLakeyras,
+      width: 2,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasTrain,
+      width: 1,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EleyrasPaysage,
+      width: 5,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: Khaneyras,
+      width: 4,
+      height: 5,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: LakeyrasArmures,
+      width: 4,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: LakeyrasAutres,
+      width: 4,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EyrasNature,
+      width: 3,
+      height: 1,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EyrasUrbain,
+      width: 2,
+      height: 1,
+      title: "truc",
+      alt: "texte"
+    },
+    {
+      src: EyrasMarchand,
+      width: 4,
+      height: 3,
+      title: "truc",
+      alt: "texte"
+    }
+  ];
+  
