763df8b4985f9e695f581e99dcc5224ff02d2f98
SalemDrenay <salemdrenay@gmail.com>
2021-02-04 22:07:40 +0100

    bug correction

--- src/components/Portfolio/Illustration/Conceptart/Drenay.jsx
+++ src/components/Portfolio/Illustration/Conceptart/Drenay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 
 import Navbar from '../../../reusables/Navbar';
 import DrenayWorldbuilding from './DrenayWorldbuilding';
