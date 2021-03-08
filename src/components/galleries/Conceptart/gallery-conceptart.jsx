7d2808f364923af52d868b18ece85973b1b6e3f8
Salem <salemdrenay@gmail.com>
2021-03-08 16:03:43 +0100

    ConceptArt texts and alts done

--- src/components/galleries/Conceptart/gallery-conceptart.jsx
+++ src/components/galleries/Conceptart/gallery-conceptart.jsx
@@ -9,36 +9,36 @@ export const photos = [
       src: Statues,
       width: 2,
       height: 1,
-      title: "truc",
-      alt: "texte"
+      title: "paysage pour l'introduction d'une émission youtube",
+      alt: "paysage orangé, un bord de falaise fait face à trois statues géantes"
     },
     {
       src: Mothmen,
       width: 3,
       height: 1,
-      title: "truc",
-      alt: "texte"
+      title: "papillons anthropomorphes",
+      alt: "quatre homme-papillons alignés"
     },
     {
       src: ButterflyDragon,
       width: 3,
       height: 2,
-      title: "truc",
-      alt: "texte"
+      title: "dragon fleuriste",
+      alt: "homme-dragon avec des ailes de paipllon sur la tête tient un bouquet derrière son dos"
     },
     {
       src: Skull,
       width: 3,
       height: 4,
-      title: "truc",
-      alt: "texte"
+      title: "concept d'ennemi pour un jeu vidéo",
+      alt: "crâne bleu avec de longs crocs et une deuxième mâchoire sur le dessus"
     },
     {
       src: Swordhand,
       width: 3,
       height: 4,
-      title: "truc",
-      alt: "texte"
+      title: "concept d'ennemi pour un jeu vidéo",
+      alt: "main sombre transpercée par une épée et avec des yeux à la place des ongles"
     }
   ];
   
